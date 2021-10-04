f5e6a79db03e5834ff431a19de5fffed1d1b2480
Orkhan Rzali <orkhan.rz8@gmail.com>
2021-10-04 13:51:38 +0400

    First version of the game
    Completed Game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
+  const time = 10;
+  const [input, setInput] = useState("");
+  const [wordCount, setWordCount] = useState(0);
+  const [timeRemaining, setTimeRemaining] = useState(time);
+  const [gameStarted, setGameStarted] = useState(false);
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setInput(value);
+  }
+
+  function startGame() {
+    setInput("");
+    setWordCount(0);
+    setTimeRemaining(time);
+    setGameStarted(true);
+  }
+
+  function endGame() {
+    setGameStarted(false);
+    let words = input.trim().split(" ").length;
+    setWordCount(words);
+  }
+
+  useEffect(() => {
+    if ((timeRemaining > 0) & gameStarted) {
+      setTimeout(() => {
+        setTimeRemaining((timeRemaining) => timeRemaining - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      endGame();
+    }
+  }, [timeRemaining, gameStarted]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Speed Typing Game</h1>
+      <textarea
+        disabled={!gameStarted}
+        placeholder="Start typing..."
+        value={input}
+        onChange={handleChange}
+      />
+      <p>Remaining time: {timeRemaining}s</p>
+      <p>Word Count: {wordCount}</p>
+      <button disabled={gameStarted} onClick={startGame}>
+        Start
+      </button>
     </div>
   );
 }
